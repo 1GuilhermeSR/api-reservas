6b4350d71613ab92826ae0ef07f14e1504cf5959
VICTOR CAETANO <victor.c.souto@academico.unirv.edu.br>
2024-05-07 14:24:42 -0300

    changed db from MySql to sqlite (in memory) also another bunch of changes and fixes

--- api.py
+++ api.py
@@ -1,75 +1,69 @@
+from flask import Flask, jsonify, request
 from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS
 
-from flask import Flask, jsonify, request, abort
 import mysql.connector
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
+db = SQLAlchemy(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+SWAGGER_URL = f'/swagger/'
+API_URL = '/static/swagger.json'
+swagger_ui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': "API-Reserva"})
+app.register_blueprint(swagger_ui_blueprint)
 
 
-def conectionDB():
-    conection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='',
-        database='reservas')
-    return conection
+class ReservaModel(db.Model):
+    __tablename__ = "reserva"
+    id = db.Column(db.Integer, primary_key=True)
+    idHotel = db.Column(db.Integer, nullable=False)
+    idQuarto = db.Column(db.Integer, nullable=False)
+    dataIni = db.Column(db.Date, nullable=False)
+    dataFin = db.Column(db.Date, nullable=False)
+
+
+def toDict(reserva):
+    return {
+        "id": reserva.id,
+        "idHotel": reserva.idHotel,
+        "idQuarto": reserva.idQuarto,
+        "dataIni": reserva.dataIni,
+        "dataFin": reserva.dataFin
+    }
 
 
 def createReserva(idhotel, idquarto, dataIni, dataFin):
-    #    if verificaDisponibilidade(idhotel, idquarto, dataIni, dataFin):
-    connection = conectionDB()
-    cursor = connection.cursor(dictionary=True)
-    sql = 'INSERT INTO reserva (idhotel, idquarto, dataIni, dataFin) VALUES (%s, %s, %s, %s)'
-    values = (idhotel, idquarto, dataIni, dataFin)
     try:
-        cursor.execute(sql, values)
-        connection.commit()
-
-        sql2 = 'SELECT * FROM reserva where id = %s'
-        values2 = (cursor.lastrowid,)
-
-        cursor.execute(sql2, values2)
-        reserva = cursor.fetchone()
+        reserva = ReservaModel(idHotel=idhotel, idQuarto=idquarto, dataIni=dataIni, dataFin=dataFin)
+        db.session.add(reserva)
+        db.session.commit()
 
         return reserva
-    except mysql.connector.Error as err:
-        return err
-    finally:
-        cursor.close()
-        connection.close()
-    #    else:
-    #        return None
+    except ValueError as err:
+        print(f"Erro ao criar a reserva: {err}")
+        # return jsonify({'error': f"Erro ao criar a reserva: {err}"}), 400
+        return None
 
 
 def consultar(idhotel, idquarto):
-    connection = conectionDB()
-    cursor = connection.cursor(dictionary=True)
-    sql = 'SELECT * FROM reserva WHERE idhotel = %s and idquarto = %s'
-    values = (idhotel, idquarto,)
     try:
-        cursor.execute(sql, values)
-        data = cursor.fetchall()
-        if data:
-            return jsonify(data), 200
-        else:
-            return jsonify({'error': 'Reserva não encontrada'}), 404
-    except mysql.connector.Error as err:
+        reservas = ReservaModel.query.filter_by(idHotel=idhotel).filter_by(idQuarto=idquarto).all()
+        return reservas
+    except ValueError as err:
         print(f"Erro ao consultar a reserva: {err}")
-        return jsonify({'error': f"Erro ao consultar a reserva: {err}"}), 400
-    finally:
-        cursor.close()
-        connection.close()
+        # return jsonify({'error': f"Erro ao consultar a reserva: {err}"}), 400
+        return None
 
 
 def verificaDisponibilidade(idhotel, idquarto, dataIni, dataFin):
-    connection = conectionDB()
-    cursor = connection.cursor(dictionary=True)
-    sql = 'SELECT dataIni, dataFin FROM reserva WHERE idhotel = %s AND idquarto = %s'
-    values = (idhotel, idquarto)
     try:
-        cursor.execute(sql, values)
-        datas = cursor.fetchall()
-    except mysql.connector.Error as err:
+        datas = (ReservaModel.query.filter_by(idHotel=idhotel).filter_by(idQuarto=idquarto)
+                 .with_entities(ReservaModel.dataIni, ReservaModel.dataFin).all())
+    except ValueError as err:
         print(err)
         return False
 
@@ -77,44 +71,41 @@ def verificaDisponibilidade(idhotel, idquarto, dataIni, dataFin):
         if hasConflict(data, dataIni, dataFin):
             return False
     return True
-    cursor.close()
-    connection.close()
 
 
 def hasConflict(data, dataIni, dataFin):
-    return ((data["dataIni"] < dataIni < data["dataFin"]) or
-            (data["dataIni"] < dataFin < data["dataFin"]) or
-            (dataIni < data["dataIni"] and dataFin > data["dataFin"]) or
-            #(dataIni > data["dataIni"] and dataFin < data["dataFin"]) or
-            (dataIni == data["dataIni"] or dataFin == data["dataFin"]))
+    return ((data[0] < dataIni < data[1]) or
+            (data[0] < dataFin < data[1]) or
+            (dataIni < data[0] and dataFin > data[1]) or
+            # (dataIni > data["dataIni"] and dataFin < data["dataFin"]) or
+            (dataIni == data[0] or dataFin == data[1]))
 
 
-def deleteReserva(id):
-    connection = conectionDB()
-    cursor = connection.cursor(dictionary=True)
-    sql = 'DELETE FROM reserva WHERE id = %s'
-    sql2 = 'SELECT * FROM reserva where id = %s'
-    values = (id,)
+def deleteReserva(idReserva):
     try:
-        cursor.execute(sql2, values)
-        data = cursor.fetchone()
-        if data:
-            cursor.execute(sql, values)
-            connection.commit()
-            return data
-        else:
+        reserva = ReservaModel.query.filter_by(id=idReserva).first()
+        if not reserva:
             return False
-    except mysql.connector.Error as err:
+        db.session.delete(reserva)
+        db.session.commit()
+        return reserva
+    except ValueError as err:
         print(f"Erro ao deletar a reserva: {err}")
         return False
-    finally:
-        cursor.close()
-        connection.close()
 
 
 @app.route('/reservar/<int:idhotel>/<int:idquarto>', methods=['GET'])
 def consultarReserva(idhotel, idquarto):
-    return consultar(idhotel, idquarto)
+    reservas = consultar(idhotel, idquarto)
+    reservaDict = []
+    for reserva in reservas:
+        reservaDict.append(toDict(reserva))
+    print(reservaDict)
+
+    if reservas:
+        return jsonify(reservaDict), 200
+    else:
+        return jsonify({'error': 'Reserva não encontrada'}), 404
 
 
 @app.route('/reservar', methods=['POST'])
@@ -144,21 +135,22 @@ def reservar():
     if not verificaDisponibilidade(idhotel, idquarto, dataInicio, dataFinal):
         return jsonify({'error': "Conflito de datas, nao foi possivel reservar"}), 409
 
-    reserva = createReserva(idhotel, idquarto, dataIni, dataFin)
+    reserva = createReserva(idhotel, idquarto, dataInicio, dataFinal)
     if reserva:
-        return jsonify(reserva), 201
-    else:
-        return jsonify({'error': 'Nao foi possivel fazer a reserva'}), 409
+        return jsonify(toDict(reserva)), 201
+    return jsonify({'error': 'Nao foi possivel fazer a reserva'}), 400
 
 
 @app.route('/reservar/<int:idreserva>', methods=['DELETE'])
 def deleteReservaEndpoint(idreserva):
     deleted_reserva = deleteReserva(idreserva)
     if deleted_reserva:
-        return jsonify(deleted_reserva), 200
+        return jsonify(toDict(deleted_reserva)), 200
     else:
-        return jsonify({'error': f"Erro ao deletar a reserva com ID {idreserva}"}), 400
+        return jsonify({'error': 'Reserva nao existe, nao foi possivel deletar'}), 404
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
